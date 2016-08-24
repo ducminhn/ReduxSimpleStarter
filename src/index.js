8f86f9bd23eb516d2cb3b46660b523c4b5216a28
Minh Nguyen <minhnguyen_1302@hotmail.com>
2016-08-23 21:32:41 -0700

    Complete ReactJS section

--- src/index.js
+++ src/index.js
@@ -1,18 +1,50 @@
-import React from 'react';
+import _ from 'lodash';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyBRbAw83DiaBxv3rFqYx61VPk70yMDsNPw';
 
-// Create a new component. This component should produce some html
-const App = () => {
-    return (
-        <div> 
-            <h1>Hello ReactJS!</h1>
-            <SearchBar />
-        </div>
-    )
+class App extends Component {
+    constructor(props){
+        super(props);
+        
+        this.state = { 
+            videos: [],
+            selectedVideo: null
+        };
+
+        this.videoSearch('Zero to hero reactjs');
+    }
+
+    videoSearch(term){
+        YTSearch({key: API_KEY, term: term}, (videos) => {
+            this.setState({ 
+                videos : videos,
+                selectedVideo: videos[0]
+            }); // ES6 feature when key and value are same name    
+        })
+    }
+
+    render(){
+
+        const videoSearch = _.debounce((term) => { this.videoSearch(term)}, 300);
+
+        return (
+            <div> 
+                <h1>Welcome to ReactJS!</h1>
+                <SearchBar onSearchTermChange={videoSearch} />
+                <VideoDetail video={this.state.selectedVideo} />
+                <VideoList 
+                    onVideoSelect = {selectedVideo => this.setState({selectedVideo})}
+                    videos={this.state.videos} />
+            </div>
+        )
+    }
 }
 
-// Take this component's generated html and put it on the page (in the DOM)
+
 ReactDOM.render(<App />, document.querySelector('.container'));
